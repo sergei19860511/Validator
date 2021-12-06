7200a56432f2c9a542ab71d31285671cf73b71e0
sergei <you@example.com>
2021-12-06 21:13:16 +0300

    create Validator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Validator
+### Простой валидатор PHP 
+Передавайте нужные Вам данные в нужные методы, валидатор всё сделает за Вас
+```php
+$email = Validator::validateString($_POST['email']);
+$password = Validator::validatePassword($_POST['password']);
+```
---
+++ Validator.php
@@ -0,0 +1,44 @@
+<?php
+
+class Validator
+{
+    private static $email;
+    private static $password;
+    private static $string;
+
+    /**
+     * @param $email
+     * @return string
+     */
+    public static function validateEmail($email)
+    {
+        self::$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        self::$email = filter_var(self::$email, FILTER_VALIDATE_EMAIL);
+        return self::$email;
+    }
+
+    /**
+     * @param $password
+     * @return string|void
+     */
+    public static function validatePassword($password)
+    {
+        if (preg_match('#^([a-zA-Z0-9]{4,12})$#i', $password)) {
+            return self::$password = $password;
+        } else{
+            FlashMessage::setFlashMessage('danger', 'пароль может быть от 4 до 12 символов латинского алфавита');
+        }
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public static function validateString($string)
+    {
+        if ($string != '') {
+            return self::$string = trim(strip_tags($string));
+        }
+        return null;
+    }
+}
